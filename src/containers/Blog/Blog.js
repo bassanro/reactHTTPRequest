3af3a48da7585b97a60c17c5e38dc81d35a63626
Roshan Bassan <roshan.bassan@gmai.com>
2019-11-26 14:49:38 +1100

    Make a http request via axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,19 +6,34 @@ import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
 import axis from "axios";
 
+// Note that axis returns a promise that can be captured via then command.
 class Blog extends Component {
+  state = {
+    posts: []
+  };
+
   componentDidMount() {
-    axis.get("https://jsonplaceholder.typicode.com/posts");
+    axis.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPost = posts.map((post) => {
+        return {
+          ...post,
+          author: "Max"
+        };
+      });
+      this.setState({ posts: updatedPost });
+      //console.log(response);
+    });
   }
 
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} author={post.author} />;
+    });
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
           <FullPost />
         </section>
