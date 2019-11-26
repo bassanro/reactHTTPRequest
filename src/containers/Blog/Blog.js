0d7998cb53aceab20c5632588d96976b53609206
Roshan Bassan <roshan.bassan@gmai.com>
2019-11-26 15:39:22 +1100

    Make a post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axis from "axios";
 // Note that axis returns a promise that can be captured via then command.
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -26,16 +27,27 @@ class Blog extends Component {
     });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} author={post.author} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
