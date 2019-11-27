f10ace948f4e8efcbf8656d97486084d67b07a5c
Roshan Bassan <roshan.bassan@gmai.com>
2019-11-27 22:59:35 +1100

    Preparing the Project for Routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,61 +1,11 @@
 import React, { Component } from "react";
 
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
-import axiosInstance from "../../axios";
+import Posts from "../Posts/Posts";
 
 // Note that axios returns a promise that can be captured via then command.
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  // For every request and response we can set global functions. Also helpful to handler errors.
-  // These are called interceptors.
-  componentDidMount() {
-    axiosInstance
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPost = posts.map((post) => {
-          return {
-            ...post,
-            author: "Max"
-          };
-        });
-
-        this.setState({ posts: updatedPost });
-        //console.log(response);
-      })
-      .catch((error) => {
-        this.setState({ error: true });
-      });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
-
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}
-          />
-        );
-      });
-    }
-
     return (
       <div className="Blog">
         <header>
@@ -70,13 +20,7 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Posts />
       </div>
     );
   }
---
+++ src/containers/Posts/Posts.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import axiosInstance from "../../axios";
+import Post from "../../components/Post/Post";
+import "./Posts.css";
+
+class Posts extends Component {
+  state = {
+    posts: []
+  };
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
+  // For every request and response we can set global functions. Also helpful to handler errors.
+  // These are called interceptors.
+  componentDidMount() {
+    axiosInstance
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPost = posts.map((post) => {
+          return {
+            ...post,
+            author: "Max"
+          };
+        });
+
+        this.setState({ posts: updatedPost });
+        //console.log(response);
+      })
+      .catch((error) => {
+        //this.setState({ error: true });
+        console.log(error);
+      });
+  }
+
+  render() {
+    let posts = <p style={{ textAlign: "center" }}>Something went wrong</p>;
+
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+      </div>
+    );
+  }
+}
+
+export default Posts;
